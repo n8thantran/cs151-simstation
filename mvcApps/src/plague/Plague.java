c0dc94621be1e4b95daae13db90b4d3a2ac03814
Donna Bui <109771688+DonnaBui@users.noreply.github.com>
2025-04-15 18:18:27 +0000

    Made some changes to Plague (ignore resistance, replace with time infected & fatality)

--- mvcApps/src/plague/Plague.java
+++ mvcApps/src/plague/Plague.java
@@ -1,32 +1,76 @@
 package plague;
 
 import mvc.Utilities;
-import prisoner.Prisoner;
 import simstation.Agent;
 import simstation.Heading;
 
 public class Plague extends Agent{
     private boolean infected;
+    private int timeInfected;
+    
     public Plague() {
         super();
         infected = false;
+        timeInfected = 0;
     }
+
     public boolean isInfected() {
         return infected;
     }
     public void setInfected(boolean infected) {
         this.infected = infected;
+        if (infected) {
+            timeInfected = world.getClock();
+        }
     }
-    // Unfinished update()
+
+    // This method tries to infect an infected Agent's neighbor.
+    public void infectNeighbor(Plague agent, Plague neighbor) {
+        if(agent.isInfected() && !neighbor.isInfected()) {
+            if (shouldInfect()) {
+                neighbor.setInfected(true);
+                PlagueSimulation.INFECTED++;
+            }
+        }
+    }
+    /* Method to determine if an agent is susceptible to infection
+    private boolean isSusceptible(Agent agent) {
+        int resistanceChance = Utilities.rng.nextInt(CHANCE);
+        return resistanceChance >= RESISTANCE;
+    }
+    */
+    
+    // Method to determine infection chance
+    private boolean shouldInfect() {
+        int infectionChance = Utilities.rng.nextInt( PlagueSimulation.MAX_CHANCE);
+        return infectionChance <  PlagueSimulation.getVirulence();
+    }
+
     @Override
     public void update() {
-        this.heading = Heading.random();
-        int steps = Utilities.rng.nextInt(10) + 1;
-        move(steps);
+
+        if (infected) {
+            int totalTime = world.getClock() - timeInfected;
+            if (totalTime >= PlagueSimulation.INFECTION_LENGTH) {
+                if (PlagueSimulation.FATAL) {
+                    this.stop(); // host dies
+                } else {
+                    infected = false;
+                    PlagueSimulation.INFECTED--;
+                }
+            }
+        }
+
+        // Attempt to infect a nearby neighbor
         Plague neighbor = (Plague) world.getNeighbor(this, 10);
         if (neighbor != null) {
             PlagueSimulation Pworld = (PlagueSimulation) world;
-            Pworld.infectNeighbor(this, neighbor);
+            this.infectNeighbor(this, neighbor);
         }
+
+        // Move randomly
+        this.heading = Heading.random();
+        int steps = Utilities.rng.nextInt(10) + 1;
+        move(steps);
     }
 }
--- mvcApps/src/plague/PlagueSimulation.java
+++ mvcApps/src/plague/PlagueSimulation.java
@@ -5,48 +5,37 @@ import simstation.*;
 
 public class PlagueSimulation extends Simulation {
     private static int VIRULENCE = 50; // % chance of infection 50
-    private static int RESISTANCE = 2; // % chance of resisting infection 2
+    // private static int RESISTANCE = 2; // % chance of resisting infection 2
     private static int POPULATION = 50;
-    private static int INFECTED;
-    public static final int CHANCE = 100;
+    public static int INFECTED;
+    public static int INFECTION_LENGTH = 50; // time until recovery
+    public static boolean FATAL = false;
+    private static int INITIAL_INFECTED = 25;
+    public static final int MAX_CHANCE = 100;
+
+    @Override
     public void populate() {
         INFECTED = 0;
-        for(int i = 0; i < POPULATION; i++)
+        for(int i = 0; i < POPULATION; i++) {
             addAgent(new Plague());
+        }
         infectAgents(); // Infect random agents in the World
     }
+
     public void infectAgents() {
         for (Agent agent : getAgents()) {
             Plague a = (Plague) agent;
-            if (!a.isInfected() && isSusceptible(agent)) {
-                    if (shouldInfect()) {
-                        a.setInfected(true);
-                        INFECTED++;
-                    }
+            if (Utilities.rng.nextInt(MAX_CHANCE) < INITIAL_INFECTED) {
+                if (!a.isInfected()) {  // if not already infected
+                    a.setInfected(true);
+                    INFECTED++;
                 }
-            }
-    }
-    // This method tries to infect an infected Agent's neighbor.
-    public void infectNeighbor(Plague agent, Plague neighbor) {
-        if(agent.isInfected() && !neighbor.isInfected() && isSusceptible(neighbor)) {
-            if (shouldInfect()) {
-                neighbor.setInfected(true);
-                INFECTED++;
             }
         }
     }
-    // Method to determine if an agent is susceptible to infection
-    private boolean isSusceptible(Agent agent) {
-        int resistanceChance = Utilities.rng.nextInt(CHANCE);
-        return resistanceChance >= RESISTANCE;
-    }
-    // Method to determine infection chance
-    private boolean shouldInfect() {
-        int infectionChance = Utilities.rng.nextInt(CHANCE);
-        return infectionChance < VIRULENCE;
-    }
+    
     public synchronized String stats() {
-        double percentInf = Math.ceil(((double) INFECTED / POPULATION) * CHANCE);
+        double percentInf = Math.ceil(((double) INFECTED / POPULATION) * MAX_CHANCE);
         return "% infected: " + percentInf;
     }
 
@@ -64,12 +53,21 @@ public class PlagueSimulation extends Simulation {
         VIRULENCE = virulence;
     }
 
+    public static int getInitialInfected() {
+        return INITIAL_INFECTED;
+    }
+    public static void setInitialInfected(int percent) {
+        INITIAL_INFECTED = percent;
+    }
+
+    /* 
     public static int getResistance() {
         return RESISTANCE;
     }
     public static void setResistance(int resistance) {
         RESISTANCE = resistance;
     }
+    */
 
     public static void main(String[] args) {
         AppPanel panel = new PlaguePanel(new PlagueFactory());
