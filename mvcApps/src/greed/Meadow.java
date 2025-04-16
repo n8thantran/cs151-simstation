bbd2aa962ec09212591f23cf476497db1d893d9d
Matilda Verdejo <matilda.verdejo@gmail.com>
2025-04-15 21:42:09 -0700

    greed: added classes

---
+++ mvcApps/src/greed/Meadow.java
@@ -0,0 +1,16 @@
+package greed;
+
+import mvc.*;
+import simstation.*;
+
+public class Meadow extends Simulation {
+    private int waitPenalty = 5;
+    private int moveEnergy = 10;
+    private int numCows = 50;
+   // private int dim = SIZE/patchSize;
+
+   // public Patch getPatch(int xc, int yc) {
+        //implement method.
+   // }
+
+}
---
+++ mvcApps/src/greed/Patch.java
@@ -0,0 +1,19 @@
+package greed;
+
+import mvc.*;
+import simstation.*;
+
+public class Patch extends Agent {
+   // private int energy <= 100;
+    private static int growBackRate = 1;
+    private int patchSize = 10;
+
+    public void eatMe(Cow cow, int amt) {
+
+    }
+
+    public void update() {
+
+    }
+
+}
